1eebe233bba7fa7476ed47b80a658d58cc8c7376
azla <azla@sitecore.net>
2023-05-18 09:33:20 +0800

    add paginating add filtering add searching add sorting

---
+++ Advanced.Web.API/Models/QueryParameters.cs
@@ -0,0 +1,36 @@
+﻿namespace Advanced.Web.Api.Models
+{
+    public class QueryParameters
+    {
+        const int _maxSize = 100;
+        private int _size = 50;
+        public int Page { get; set; } = 1;
+        public int Size
+        {
+            get { return _size; }
+            set
+            {
+                _size = Math.Min(_maxSize, value);
+            }
+        }
+
+        public string SortBy { get; set; } = "Id";
+        public string _sortOrder = "asc";
+        public string SortOrder
+        {
+            get 
+            { 
+                return _sortOrder; 
+            }
+            set 
+            { 
+                if (value == "asc" || value == "desc")
+                {
+                    _sortOrder = value;
+                }                
+            }
+        }
+        
+
+    }
+}
